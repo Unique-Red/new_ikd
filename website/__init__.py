b3f7d838cf87292b08e199f2f4ee698d4aed60b8
Redx <78570858+Unique-Red@users.noreply.github.com>
2022-01-13 17:14:55 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,7 @@ from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "helloworld"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://modbbwxczgreni:2d84f5ad03a054888be482357c79cc7d47ef3887326d72f2eafb0fc2fa55d386@ec2-54-89-105-122.compute-1.amazonaws.com:5432/dbu5dstpmb5r3s'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comfy.db'
 
 db = SQLAlchemy(app)
 ckeditor = CKEditor(app)
